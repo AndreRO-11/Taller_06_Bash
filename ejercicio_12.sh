5fc3a75c3f21631d21cc8a32690b391489051b37
Scarlett <taylok11@gmail.com>
2024-11-10 01:15:26 -0300

    Ejercicio 12

--- ejercicio_12.sh
+++ ejercicio_12.sh
@@ -6,4 +6,13 @@
 # Escriba un script que ordene los nombres y elimine los duplicados.
 # Guarde el resultado en sorted_names.txt.
 
+archivo_nombres="names.txt"
 
+if [ ! -f "$archivo_nombres" ]; then
+    echo "Error: El archivo '$archivo_nombres' no existe."
+    exit 1
+fi
+
+sort "$archivo_nombres" | uniq > sorted_names.txt
+
+echo "Los nombres ordenados y sin duplicados se han guardado en sorted_names.txt."
