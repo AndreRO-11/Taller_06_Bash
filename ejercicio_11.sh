cfb4429d2ec880c9a21a573ab3656a2ed041329a
Scarlett <taylok11@gmail.com>
2024-11-10 01:23:19 -0300

    Ejercicio 11

--- ejercicio_11.sh
+++ ejercicio_11.sh
@@ -5,3 +5,6 @@
 # Crear un script que registre la fecha y hora actuales en un archivo cada minuto.
 # Configurar un trabajo cron para ejecutar este script.
 
+logfile="logfile.txt"
+
+echo "$(date)" >> "$logfile"
